68c37cdee943e4140d6fe4e1aa00cf122bcbd683
Andrey Toporkov <toporkov@karas.karas>
2015-02-22 13:02:16 +0300

    Committer: Andrey Toporkov <toporkov@karas.karas>  On branch master  Initial commit

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 15 21:50:12 2015
+
+@author: toporkov
+"""
+
+CSRF_ENABLED=True
+SECRET_KEY='you-will-never-guess'
---
+++ run.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 13:04:42 2015
+
+@author: toporkov
+"""
+
+#!flask/bin/python
+from app import app
+app.run(debug=True)
